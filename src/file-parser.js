875d9264d9d6c3d79d5b3afb8114c6af0acbc787
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2022-01-25 23:49:09 -0500

    refactor: update description

--- src/file-parser.js
+++ src/file-parser.js
@@ -1,9 +1,28 @@
 /**
- * Function to parse any given file size, convert it into
- * mbs and then return the result
+ * We want to be able to convert a given file size ( string or number )
+ * of any valid unit of measure ( "gb", "mb", "kb", etc ) into a floating
+ * point number that represents the file size in mbs
  *
- * @param  {string} file-size - file size to be converted
- * @return {float}          - file size in mbs
+ * The fileParser function should work like this:
+ *
+ * fileParser("100kb")     ---> 0.1
+ * fileParser("100mb")     ---> 100.00
+ * fileParser("100gb")     ---> 102400.00
+ * fileParser("100tb")     ---> 104857600.00
+ * fileParser("   100kb")  ---> 0.1
+ * fileParser("100    kb") ---> 0.1
+ * fileParser("100kb   ")  ---> 0.1
+ *
+ * Also, any invalid input (format or unit) should result in an error.
+ * For example:
+ *
+ * fileParser()           ---> Error
+ * fileParser("whatever") ---> Error
+ * fileParser("100wba")   ---> Error
+ * fileParser("110al0")   ---> Error
+ *
+ * @param  {string | number} file-size - file size to be converted
+ * @return {number}
  */
 module.exports = function (fileSize) {
   // ...
