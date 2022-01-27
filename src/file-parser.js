68eed2164e06c747b77cb4fbb661b589fe542f52
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2022-01-27 13:50:54 -0500

    refactor: update function jsdoc

--- src/file-parser.js
+++ src/file-parser.js
@@ -1,11 +1,12 @@
 /**
- * We want to be able to convert a given file size ( string or number )
+ * We want to be able to convert a given file size ( string or number, uppercase or lowercase)
  * of any valid unit of measure ( "gb", "mb", "kb", etc ) into a floating
  * point number that represents the file size in mbs
  *
  * The fileParser function should work like this:
  *
  * fileParser("100kb")     ---> 0.09765625
+ * fileParser("100KB")     ---> 0.09765625
  * fileParser("100mb")     ---> 100.00
  * fileParser("100gb")     ---> 102400.00
  * fileParser("100tb")     ---> 104857600.00
@@ -21,7 +22,7 @@
  * fileParser("100wba")   ---> Error
  * fileParser("110al0")   ---> Error
  *
- * @param  {string} file-size - file size to be converted
+ * @param  {string} file-size - file size to be converted ( case insensitive )
  * @return {number}
  */
 module.exports = function (fileSize) {
